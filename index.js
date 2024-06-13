42195636d8a6cad790253171f9e481302aa0b87c
Riccardo Spadon <spadon.riccardo@gmail.com>
2024-06-13 17:48:36 +0200

    exercises
    Exercise 5 done!

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // 1 Scrivi un programma che calcoli l'area di un rettangolo.
 // Chiedi all'utente di inserire la lunghezza e la larghezza del rettangolo e quindi calcola e stampa l'area.
-let lunghezza = prompt('Inserisci la lunghezza del rettangolo: ') // 5
-let larghezza = prompt('Inserisci la larghezza del rettangolo: ') // 6
+let lunghezza = 5 // prompt('Inserisci la lunghezza del rettangolo: ')
+ let larghezza = 6 // prompt('Inserisci la larghezza del rettangolo: ') 
 let area = lunghezza * larghezza
 console.log("L'area del rettangolo avendo lunghezza", lunghezza, "e larghezza", larghezza, "è:", area)
 // L'area del rettangolo avendo lunghezza 5 e larghezza 6 è: 30
+
 
 // 2 Scrivi una funzione chiamata isPalindrome che prenda una stringa come input e restituisca true se la stringa è un palindromo
 // (cioè può essere letta allo stesso modo sia da sinistra a destra che da destra a sinistra), altrimenti restituisca false.
@@ -13,6 +14,7 @@ const isPalindrome = (string) => {
 }
 console.log("La parola 'ciao' è palindroma?", isPalindrome('ciao')) // La parola 'ciao' è palindroma? false
 console.log("La parola 'anna' è palindroma?", isPalindrome('anna')) // La parola 'anna' è palindroma? true
+
 
 // 3 Scrivi una funzione chiamata findMax che prenda un array di numeri come input e restituisca il numero più grande nell'array
 const array = [1, 2, 3, 4, 5, 65, 7, 8, 9, 10]
@@ -27,4 +29,20 @@ const findMax = (array) => {
 }
 console.log(findMax(array)) // 65
 
-// 4
+
+// 4 Scrivi un programma che stampi i primi 10 numeri della sequenza di Fibonacci.
+// La sequenza di Fibonacci è una serie di numeri in cui ogni numero è la somma dei due numeri precedenti
+// (ad esempio, 0, 1, 1, 2, 3, 5, 8, 13, ...).
+const fibonacci = (n) => {
+    let sequenza = [0,1]
+    for(let i = 2; i < n; i++){
+        let nextNumber =  sequenza[i-1] + sequenza[i-2]
+        sequenza.push(nextNumber)
+    }
+    return sequenza
+}
+const primiDieciFibonacci = fibonacci(10)
+console.log(primiDieciFibonacci) // [0, 1,  1,  2,  3, 5, 8, 13, 21, 34]
+
+
+// 5
