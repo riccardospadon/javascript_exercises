bdf13b6af86479fd7e875df67894c031ced0f478
Riccardo Spadon <spadon.riccardo@gmail.com>
2024-06-20 10:28:37 +0200

    exercises
    Exercises 19 and 20 done!!

--- index.js
+++ index.js
@@ -203,3 +203,38 @@ verifyName = verifyName.toString()
 console.log(typeof verifyName) // string
 verifyName = name1.name.toLowerCase() === name2.name.toLowerCase()
 console.log(verifyName) // true
+
+
+// 19 Scrivi un algoritmo per trovare il più grande tra due numeri interi
+let c = 10
+let d = 10
+if(c > d) {
+    console.log(`Il numero c: ${c} è maggiore del numero d: ${d}`)
+} else if(c < d) {
+    console.log(`Il numero c: ${c} è minore del numero d: ${d}`)
+} else if (c === d) {
+    console.log(`Il numero c: ${c} è uguale al numero d: ${d}`)
+}
+
+
+// 20 Crea un blocco condizionale if/else per mostrare in console il messaggio corretto in ogni condizione:
+// num < 5 - "Tiny"; num < 10 - "Small"; num < 15 - "Medium"; num < 20 - "Large"; num >= 20 - "Huge"
+let num = 23
+if (num < 5){
+    console.log('Tiny')
+}
+else if (num >= 5 && num < 10){
+    console.log('Small')
+}
+else if (num >= 10 && num < 15){
+    console.log('Medium')
+}
+else if (num >= 15 && num < 20){
+    console.log('Large')
+}
+else if (num >= 20){
+    console.log('Huge')
+}
+
+
+// 
